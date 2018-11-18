b643e8284389e27af1b85e0418d5fa9d68161dc7
Jakob Turk <jakob.turk@gmail.com>
2018-11-18 07:49:14 +0100

    Upload MathUtils

---
+++ src/MathUtils.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////
+// MathUtils.js______________________________
+// This is a collection of simple and quick
+// math functions used throughout the program
+/////////////////////////////////////////////
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
