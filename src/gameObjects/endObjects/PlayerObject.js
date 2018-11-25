8dd59f4c1e6fb0e04bcee30b4faad87fe19f3229
Jakob Turk <jakob.turk@gmail.com>
2018-11-25 15:10:50 +0100

    Fix last nights missing index.html commit

--- src/gameObjects/endObjects/PlayerObject.js
+++ src/gameObjects/endObjects/PlayerObject.js
@@ -8,7 +8,7 @@ function PlayerObject (controls) {
 
 	this.loadModel("assets/models/kurent.json");
 
-	this.setPosition([5.0, 1.0, 3.0]);
+	this.setPosition([5.0, this.objHeight / 2.0, 3.0]);
 	this.setAngle(0.0);
 	this.setYaw(0.0);
     this.setSpeed(8.0);
@@ -20,13 +20,13 @@ PlayerObject.prototype.handleLoadedModel = function (data) {
 	
 	this.vertices = data.vertices;
     this.colors = [
-        [0.937, 0.902, 0.827, 1.0] // Beige
+        [0.937, 0.902, 0.727, 1.0] // Beige?
     ];
     this.nVertices = this.vertices.length / 3;
 
     this.vertexIndices = data.faces;
 	this.nVertexIndices = this.vertexIndices.length;
-
+	
     this.findHeight();
     this.findRadius();
     GRAPHICS.loadObjectVertices(this);
@@ -53,7 +53,7 @@ PlayerObject.prototype.control = function (elapsedTime) {
 PlayerObject.prototype.controlCamera = function () {
 
 	// Set up relative position of camera and rotate around player by yaw
-	var cameraPosition = vec3.fromValues(0.0, 20.0, -25.0);
+	var cameraPosition = vec3.fromValues(0.0, 10.0, -20.0);
 	var cameraYaw = 180.0;
 
 	if (!document.getElementById("fixCamera").checked){
@@ -64,13 +64,13 @@ PlayerObject.prototype.controlCamera = function () {
 		vec3.add(cameraPosition, cameraPosition, this.getPosition());
 
 		cameraYaw -= this.getYaw();
-	}
 
-	// Move on top of terrain if camera is inside terrain
-	var landscapeCameraHeight = GAME_OBJECT_MANAGER.getLandscape().getHeight(cameraPosition[0], cameraPosition[2]);
-	var landscapePlayerHeight = GAME_OBJECT_MANAGER.getLandscape().getHeight(this.getPosition()[0], this.getPosition()[2]);
-	if (landscapeCameraHeight - landscapePlayerHeight > 5.0) {
-		cameraPosition[1] = landscapeCameraHeight + 1;
+		// Move on top of terrain if camera is inside terrain
+		var landscapeCameraHeight = GAME_OBJECT_MANAGER.getLandscape().getHeight(cameraPosition[0], cameraPosition[2]);
+		var landscapePlayerHeight = GAME_OBJECT_MANAGER.getLandscape().getHeight(this.getPosition()[0], this.getPosition()[2]);
+		if (landscapeCameraHeight - landscapePlayerHeight > 5.0) {
+			cameraPosition[1] = landscapeCameraHeight + 1;
+		}
 	}
 
 	// Set viewport
