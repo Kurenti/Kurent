8c2bd1d17ad233208f33dfa0a043e63310cd2a52
Anc0 <ak6688@student.uni-lj.si>
2018-11-30 09:57:08 +0100

    Start work on bells.

---
+++ src/gameObjects/endObjects/BellObject.js
@@ -0,0 +1,175 @@
+/////////////////////////////////////
+// BellObject.js________________________
+// Bell that is appended to our Kurent
+/////////////////////////////////////
+
+function BellObject (startingPosition, playerObject) {
+    this.playerObject = playerObject;
+    this.scale = 0.05;
+    this.attached = true;
+    this.loadVertices();
+
+    this.setPosition(startingPosition);
+    this.setSpeed(this.playerObject.speed);
+    this.setAngularSpeed(this.playerObject.angularSpeed);
+    this.setAngle(this.playerObject.angle);
+    this.setYaw(this.playerObject.yaw);
+}
+BellObject.prototype = new MovableObject();
+
+BellObject.prototype.loadVertices = function () {
+
+    this.vertices = [
+        // Front face
+        -1.0, -1.0,  1.0,
+        1.0, -1.0,  1.0,
+        1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+        1.0,  1.0,  1.0,
+        1.0,  1.0, -1.0,
+
+        // Bottom face
+        -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0
+    ];
+    this.vertexNormals = [
+        // Front face
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+
+        // Back face
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+
+        // Top face
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+
+        // Bottom face
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+
+        // Right face
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+
+        // Left face
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0
+    ];
+    this.colors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [1.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 1.0, 0.0, 1.0], // Top face
+        [1.0, 0.5, 0.5, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 0.0, 1.0, 1.0]  // Left face
+    ];
+    this.textureCoords = [
+        // Front face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+
+        // Back face
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+
+        // Top face
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+
+        // Bottom face
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+
+        // Right face
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        0.0, 0.0,
+
+        // Left face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
+    ];
+    this.nVertices = 24;
+
+    this.vertexIndices = [
+        0, 1, 2,      0, 2, 3,    // Front face
+        4, 5, 6,      4, 6, 7,    // Back face
+        8, 9, 10,     8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15, // Bottom face
+        16, 17, 18,   16, 18, 19, // Right face
+        20, 21, 22,   20, 22, 23  // Left face
+    ];
+    this.nVertexIndices = 36;
+
+    this.vertices = this.vertices.map( function (x) {
+        return x*this.scale;
+    }.bind(this));
+
+    GRAPHICS.loadObjectVertices(this);
+};
+
+BellObject.prototype.update = function (elapsedTime) {
+    if(this.attached) {
+        this.setPosition([this.playerObject.position[0] + 1, this.playerObject.position[1], this.playerObject.position[2] + 1]);
+        this.setAngle(this.playerObject.angle);
+        this.setYaw(this.playerObject.yaw);
+    }
+    console.log(this.position);
+};
+
+BellObject.prototype.attach = function() {
+  this.attached = true;
+};
+
+BellObject.prototype.unattach = function() {
+  this.attached = false;
+};
